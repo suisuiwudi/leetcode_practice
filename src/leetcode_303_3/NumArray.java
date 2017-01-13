dfc64303a88e5af030851ca9d90ca0874dc39924
suisuiwudi <SHURUI@shuruideMacBook-Pro.local>
2017-01-13 15:49:29 -0800

    test

---
+++ src/leetcode_303_3/NumArray.java
@@ -0,0 +1,14 @@
+package leetcode_303_3;
+
+public class NumArray {
+	private int add[];
+	public NumArray(int[] nums){
+		for ( int i = 1; i<nums.length; i++)
+			nums[i]=nums[i-1]+nums[i];
+		this.add = nums;
+	}
+	public int sumRange(int i, int j){
+		if (i>0) return add[j]-add[i-1];
+		return add[j];
+	}
+}
