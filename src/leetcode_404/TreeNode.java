c085e8e95f8347a91b2d995e8127ac352d3ae3c7
suisuiwudi <suisuiwudi@gmail.com>
2017-01-22 17:32:04 -0800

    leetcode_404

---
+++ src/leetcode_404/TreeNode.java
@@ -0,0 +1,11 @@
+package leetcode_404;
+
+/**
+ * Definition for a binary tree node.
+ */
+  public class TreeNode {
+      int val;
+     TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+  }
