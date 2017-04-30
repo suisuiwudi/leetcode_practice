5519be843688419d85870291b5918752a1394f6b
Rui <suisuiwudi@gmail.com>
2017-04-30 02:15:09 -0700

    leetcode_523, discrete number by using map

---
+++ src/leetcode_523/Solution.java
@@ -0,0 +1,23 @@
+package leetcode_523;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    public boolean checkSubarraySum(int[] nums, int k) {
+        Map<Integer,Integer> map = new HashMap();
+        map.put(0,-1);
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++){
+            sum += nums[i];
+            if (k != 0) sum %= k;
+            if (map.containsKey(sum)){
+                if (i - map.get(sum) > 1) return true;
+            } else {
+                map.put(sum,i);
+            }
+        }
+        return false;
+    }
+    
+}
