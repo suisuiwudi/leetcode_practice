74c9c89b540d8a630fd182436784408f996fd978
Rui <suisuiwudi@gmail.com>
2017-06-13 23:37:19 -0700

    leetcode_100

---
+++ src/leetcode_100/Solution.java
@@ -0,0 +1,21 @@
+package leetcode_100;
+
+import data_structure.TreeNode;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+        if (p.val != q.val || !isSameTree(p.right,q.right) || !isSameTree(p.left,q.left)) return false;
+        return true;
+    }
+}
