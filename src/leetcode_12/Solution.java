62cfb388c3d7e9f22b161475b2a9b5504c77dac5
Rui <suisuiwudi@gmail.com>
2017-06-20 00:39:27 -0700

    leetcode_12, roman number

--- src/leetcode_12/Solution.java
+++ src/leetcode_12/Solution.java
@@ -3,46 +3,15 @@ package leetcode_12;
 public class Solution {
     public String intToRoman(int num) {
         StringBuilder sb = new StringBuilder();
+        String[] roman = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        int[] number = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
         while (num!=0){
-            if (num >= 1000){
-                sb.append("M");
-                num-=1000;
-            } else if (num >= 900){
-                sb.append("CM");
-                num-=900;
-            } else if (num >= 500){
-                sb.append("D");
-                num-=500;
-            } else if (num >= 400){
-                sb.append("CD");
-                num-=400;
-            } else if (num >= 100){
-                sb.append("C");
-                num-=100;
-            } else if (num >= 90){
-                sb.append("XC");
-                num-=90;
-            } else if (num >= 50){
-                sb.append("L");
-                num-=50;
-            } else if (num >= 40){
-                sb.append("XL");
-                num-=40;
-            } else if (num >= 10){
-                sb.append("X");
-                num-=10;
-            } else if (num >= 9){
-                sb.append("IX");
-                num-=9;
-            } else if (num >= 5){
-                sb.append("V");
-                num-=5;
-            } else if (num >= 4){
-                sb.append("IV");
-                num-=4;
-            } else {
-                sb.append("I");
-                num--;
+            for (int i = 0; i < roman.length; i++){
+                if (num >= number[i]){
+                    sb.append(roman[i]);
+                    num-=number[i];
+                    break;
+                }
             }
         }
         return sb.toString();
