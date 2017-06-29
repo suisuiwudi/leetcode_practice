903725c17cff0ca3f3b420357e9c6ceee87eae80
Rui <suisuiwudi@gmail.com>
2017-06-28 23:45:58 -0700

    leetcode_26

---
+++ src/leetcode_26/Solution.java
@@ -0,0 +1,11 @@
+package leetcode_26;
+
+public class Solution {
+    public int removeDuplicates(int[] nums) {
+        int index = 1;
+        for (int i = 1; i < nums.length; i++){
+            if (nums[i] != nums[i-1])nums[index++] = nums[i];
+        }
+        return index;
+    }
+}
