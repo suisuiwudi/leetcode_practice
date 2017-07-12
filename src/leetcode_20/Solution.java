477756dbb46b0f20e13d06a03753357da706ee37
Rui <suisuiwudi@gmail.com>
2017-07-11 22:42:33 -0700

    leetcode_20

---
+++ src/leetcode_20/Solution.java
@@ -0,0 +1,21 @@
+package leetcode_20;
+
+import java.util.Stack;
+
+public class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack();
+        for (char ch : s.toCharArray()){
+            if (ch == '[' ){
+                stack.push(']');
+            } else if (ch == '('){
+                stack.push(')');
+            } else if (ch == '{'){
+                stack.push('}');
+            } else if (stack.isEmpty() || stack.pop() != ch){
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
