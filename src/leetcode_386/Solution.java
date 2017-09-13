e09fb8ea0c64362e4277ca31756538de2d35c7cd
Rui <suisuiwudi@gmail.com>
2017-09-13 13:52:30 -0700

    leetcode_386, find next number one by one

---
+++ src/leetcode_386/Solution.java
@@ -0,0 +1,25 @@
+package leetcode_386;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<Integer> lexicalOrder(int n) {
+        int next = 1;
+        List<Integer> list = new ArrayList();
+        for (int i = 0; i < n; i++){
+            list.add(next);
+            if (next * 10 <= n){
+                next *= 10;
+            } else if (next + 1 <= n && next % 10 != 9){
+                next++;
+            } else {
+                while ((next / 10) % 10 == 9){
+                    next = next / 10;
+                }
+                next = next / 10 + 1;
+            }
+        }
+        return list;
+    }
+}
