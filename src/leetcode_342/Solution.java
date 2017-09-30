b5ba618f60e0c10af3feaa11674d7252bdc6fe4f
Rui <suisuiwudi@gmail.com>
2017-09-29 22:38:54 -0700

    leetcode_342, the 1 bit would be only at odd position

---
+++ src/leetcode_342/Solution.java
@@ -0,0 +1,7 @@
+package leetcode_342;
+
+class Solution {
+    public boolean isPowerOfFour(int num) {
+        return (num > 0) && ((num & (num - 1)) == 0) && ((num & 0x55555555) == num);
+    }
+}
