9da6f6dd1f727f681430e7568701149fa385ab0c
Rui <suisuiwudi@gmail.com>
2017-10-01 01:29:11 -0700

    leetcode_453, making n-1 elements increase by 1 equals making 1 elements decrease by 1

---
+++ src/leetcode_453/Solution.java
@@ -0,0 +1,15 @@
+package leetcode_453;
+
+class Solution {
+    public int minMoves(int[] nums) {
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < nums.length; i++){
+            if (nums[i] < min) min = nums[i];
+        }
+        int ans = 0;
+        for (int i = 0; i < nums.length; i++){
+            ans += nums[i] - min;
+        }
+        return ans;
+    }
+}
